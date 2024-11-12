4ebd2c4f54faa624b1834b5bbc1a68dce89c2bb0
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-11-12 16:23:18 +0530

    merged

--- src/utils/zodSchemas.ts
+++ src/utils/zodSchemas.ts
@@ -1,20 +1,25 @@
 import { z } from "zod";
 
 const loginSchema = z.object({
-	email: z.string(),
-	password: z.string(),
+  email: z.string(),
+  password: z.string(),
 });
 
 const registerSchema = z.object({
-	firstName: z.string().min(3),
-	lastName: z.string().optional(),
-	email: z.string().email(),
-	password: z.string().min(4),
+  firstName: z.string().min(3),
+  lastName: z.string().optional(),
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+const editUserDetails = z.object({
+  firstName: z.string().min(3),
+  lastName: z.string().optional(),
+  email: z.string().email(),
 });
 
 const otpSchema = z.object({
-	email: z.string().email(),
-	otp: z.string(),
+  email: z.string().email(),
+  otp: z.string(),
 });
 
-export { loginSchema, registerSchema, otpSchema };
+export { loginSchema, registerSchema, otpSchema, editUserDetails };
