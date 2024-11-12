ce2e6a755b38b8a4d5b57cb06bdd892763dd04b0
Sahad <zanusahad@gmail.com>
2024-11-12 09:49:07 +0530

    add signin and signup functionality (#2)
    * create userSchema
    
    * create signin and signup controller

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
@@ -12,18 +13,19 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-	res.send("Hello World!");
+  res.send("Hello World!");
 });
 
+app.use("/auth", authRoutes);
 app.use(globalErrorHandler);
 
 mongoose
-	.connect(process.env.MONGO_URI || "")
-	.then(() => {
-		app.listen(port, () => {
-			console.log(`Server is running on port ${port}`);
-		});
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+  .connect(process.env.MONGO_URI || "")
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ src/models/userModel.ts
@@ -0,0 +1,33 @@
+import mongoose, { type Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const userSchema: Schema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", userSchema);
