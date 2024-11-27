d480c59a7b627d5d16a9a037bf31e928fbab4b04
sinan <sinanbh9605@gmail.com>
2024-11-27 10:39:15 +0530

    updated search controller to only include user's worksapce

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,18 +1,23 @@
-import type { Request, Response } from "express";
+import type { Response } from "express";
 import { CustomError } from "../utils/error/customError";
 import { StandardResponse } from "../utils/standardResponse";
 import Workspace from "../models/workspaceModel";
+import type { CustomRequest } from "../types/interfaces";
+import mongoose from "mongoose";
 
-export const searchWorkspace = async (req: Request, res: Response) => {
+export const searchWorkspace = async (req: CustomRequest, res: Response) => {
+	const userId = req.user?.id;
 	const { query } = req.query;
 
 	if (!query || typeof query !== "string") {
 		throw new CustomError("Query parameter is required and should be a string", 400);
 	}
+
 	const workspaces = await Workspace.aggregate([
 		{
 			$match: {
 				name: { $regex: query, $options: "i" },
+				members: { $in: [new mongoose.Types.ObjectId(userId)] },
 			},
 		},
 	]);
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -8,7 +8,7 @@ export const app = express();
 export const server = createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: process.env.CLIENT_URL,
 		methods: ["GET", "POST"],
 	},
 });
