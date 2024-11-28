b88178c02fab14c87dc7356f82ff39491812b976
sinan <sinanbh9605@gmail.com>
2024-11-28 09:51:21 +0530

    updated socket: only can chat joined workspace

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -2,7 +2,9 @@ import Chat from "../models/chatModel";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const app = express();
 
 export const server = createServer(app);
@@ -15,6 +17,12 @@ const io = new Server(server, {
 
 io.on("connection", async (socket) => {
 	if (process.env.NODE_ENV === "development") console.log("User connected");
+
+	socket.on("joinWorkspace", (workSpaceId) => {
+		socket.join(workSpaceId);
+		if (process.env.NODE_ENV === "development") console.log(`User joined workspace: ${workSpaceId}`);
+	});
+
 	socket.on("sendMessage", async (message) => {
 		try {
 			const chat = await Chat.findOne({ workspaceId: message?.workSpaceId });
@@ -29,7 +37,7 @@ io.on("connection", async (socket) => {
 				const updatedChat = await chat.save();
 				const populatedChat = await updatedChat.populate("messages.sender");
 
-				io.emit("receiveMessage", populatedChat);
+				io.to(message?.workSpaceId).emit("receiveMessage", populatedChat);
 			} else {
 				const newChat = new Chat({
 					workspaceId: message?.workSpaceId,
@@ -43,7 +51,7 @@ io.on("connection", async (socket) => {
 				});
 
 				const savedMessage = await (await newChat.save()).populate("messages.sender");
-				io.emit("receiveMessage", savedMessage);
+				io.to(message?.workSpaceId).emit("receiveMessage", savedMessage);
 			}
 		} catch (err) {
 			console.error("Error saving message to database:", err);
